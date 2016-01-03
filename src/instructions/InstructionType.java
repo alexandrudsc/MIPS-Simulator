ed204743cc82650ff1a4c202619c72eec1b429d3
alexandrudsc <alexandru.dascalu94@gmail.com>
2016-01-03 20:45:11 +0200

    added NetBeans proj structure

--- src/instructions/InstructionType.java
+++
@@ -1,9 +0,0 @@
-package instructions;
-
-public enum InstructionType {
-	add, addi, sub, 
-	lw, lh, lhu, lb, lbu, sw, sh, sb, lui,
-	sll, srl, and, andi, or, ori, nor,
-	beq, bne, j, jal, jr,
-	slt
-}
